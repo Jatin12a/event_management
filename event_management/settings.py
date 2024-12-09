0e76d14bae71e9457735bf030779483eaf92de02
Jatin Gupta <92937296+Jatin12a@users.noreply.github.com>
2024-12-10 00:27:38 +0530

    Pushed All at once

--- event_management/settings.py
+++ event_management/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eventmanagement',
+        'USER': 'root',
+        'PASSWORD': 'Jatinjoker1@',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
